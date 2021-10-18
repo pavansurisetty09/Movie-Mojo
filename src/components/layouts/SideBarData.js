f697382ee09ae869f6c02009dd6a30b25bc7f5d4
rishirhyspavan <surisettypavankumar999@gmail.com>
2021-10-18 14:57:17 +0530

    routes added for github pages

--- src/components/layouts/SideBarData.js
+++ src/components/layouts/SideBarData.js
@@ -10,7 +10,7 @@ import * as RiIcons from "react-icons/ri";
 export const SideBarData = [
   {
     title: "Movies",
-    path: "/movies",
+    path: "/Movie-Mojo/movies",
     cName: "menu-text",
     icon: <BiIcons.BiCameraMovie />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
@@ -18,31 +18,31 @@ export const SideBarData = [
     items: [
       {
         title: "Trending",
-        path: "/movies/trending",
+        path: "/Movie-Mojo/movies/trending",
         icon: <AiIcons.AiFillFire />,
       },
       {
         title: "Popular",
-        path: "/movies/popular",
+        path: "/Movie-Mojo/movies/popular",
         icon: <AiIcons.AiFillStar />,
       },
     ],
   },
   {
     title: "TV",
-    path: "/tv",
+    path: "/Movie-Mojo/tv",
     icon: <BsIcons.BsFillDisplayFill />,
     cName: "menu-text",
   },
   {
     title: "Music",
-    path: "/music",
+    path: "/Movie-Mojo/music",
     icon: <FaIcons.FaMusic />,
     cName: "menu-text",
   },
   {
     title: "News",
-    path: "/news",
+    path: "/Movie-Mojo/news",
     cName: "menu-text",
     icon: <Io5Icons.IoNewspaperOutline />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
@@ -50,37 +50,37 @@ export const SideBarData = [
     items: [
       {
         title: "Trending",
-        path: "/news/trending",
+        path: "/Movie-Mojo/news/trending",
         icon: <AiIcons.AiFillFire />,
       },
       {
         title: "Live",
-        path: "/news/live",
+        path: "/Movie-Mojo/news/live",
         icon: <MdIcons.MdLiveTv />,
       },
     ],
   },
   {
     title: "Artists",
-    path: "/artists",
+    path: "/Movie-Mojo/artists",
     icon: <MdIcons.MdPeople />,
     cName: "menu-text",
   },
   {
     title: "Reviews",
-    path: "/reviews",
+    path: "/Movie-Mojo/reviews",
     icon: <MdIcons.MdRateReview />,
     cName: "menu-text",
   },
   {
     title: "Videos",
-    path: "/videos",
+    path: "/Movie-Mojo/videos",
     icon: <MdIcons.MdSlowMotionVideo />,
     cName: "menu-text",
   },
   {
     title: "Entertainment",
-    path: "/entertainment",
+    path: "/Movie-Mojo/entertainment",
     icon: <MdIcons.MdLiveTv />,
     cName: "menu-text",
   },
